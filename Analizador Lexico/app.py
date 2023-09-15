4296cb0f3e508feead532eaeabb884ff72bfd9eb
Adrianシロ <adrianstratos@gmail.com>
2023-09-14 19:42:12 -0500

    Adelanto chiquito

---
+++ Analizador Lexico/app.py
@@ -0,0 +1,32 @@
+Codigo = []
+
+with open('archivo.c', 'r') as archivo:
+    for linea in archivo:
+        linea = linea.strip();
+        if linea:
+            Codigo.append(linea)
+    for linea in Codigo:
+        #Codigo para palabras reservadas
+
+        #Codigo para Identificadores
+
+        #Codigo para Comentarios
+
+        #Codigo para Numeros enteros
+
+        #Codigo para Numeros flotantes
+
+        #Codigo para Simbolos especiales
+
+        #Codigo para operadores logicos
+
+        #Codigo para operadores matematicos
+
+        #Codigo para operadores de comparacion
+
+        #Codigo para cadenas
+
+        #Codigo para declarar funciones?
+
+        #Codigo para parametros?
+        print("linea: " + linea)            
