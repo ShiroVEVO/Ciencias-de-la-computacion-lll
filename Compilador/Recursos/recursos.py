0bf8402b5f28fe8a0bcbbed68127d93b14c08b88
Adrianシロ <adrianstratos@gmail.com>
2023-09-21 20:50:30 -0500

    validador de bloques

--- Compilador/Recursos/recursos.py
+++ Compilador/Recursos/recursos.py
@@ -17,7 +17,18 @@ def contar_elemento(lista, campo):
 
     return contador
 
-def leer_archivo(ruta_archivo):
+"""
+Recibe por parametro una ruta absoluta "ruta_archivo" desde la raiz del proyecto es decir, 
+Para rutas internas del Proyecto del compilador requiere que se especifique ese directorio
+al principio de la ruta por ejemplo:
+    Compilador/Analizado_Lexico/tokens.py
+Si es en la carpeta general de compilador: 
+    Compilador/archivo.c
+luego de leerlo, omite las tabulaciones y espacios iniciales asi como todas las lineas que
+sean de separación y las va introduciendo en un arreglo "codigo" el cual devolverá una vez
+termine de leer el archivo.
+"""
+def leer_archivo(ruta_archivo): 
     codigo = []
     with open(ruta_archivo, 'r') as archivo:
         for linea in archivo:
